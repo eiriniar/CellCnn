ab44c45675b401609980bc2fa779f177e58b7a5c
eiriniar <arvaniti@imsb.biol.ethz.ch>
2017-02-13 21:28:17 +0100

    added .nojekyll

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="CellCnn",
-    version="0.1",
-    author="Eirini Arvaniti",
-    description="Convolutional neural network for analyzing single cell measurements",
-    packages=find_packages(),
-)
