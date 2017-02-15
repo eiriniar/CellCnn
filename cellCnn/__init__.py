49de982f685c5daab2ef37599709c78439031664
eiriniar <arvaniti@imsb.biol.ethz.ch>
2017-02-15 13:25:16 +0100

    upgraded Theano andset optimizer='fast_compile' to avoid NaN during optimization

--- cellCnn/__init__.py
+++ cellCnn/__init__.py
@@ -1,1 +1,7 @@
 
+# according to this issue: https://github.com/fchollet/keras/issues/828
+# setting the optimizer to `fast_compile` avoids NaNs when training with ReLU
+
+import theano
+theano.config.optimizer = 'fast_compile'
+print theano.config.optimizer
